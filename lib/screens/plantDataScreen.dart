81bf37bb2655887516dc9bf9169c905d7a7e7f16
Maqcel <56508769+Maqcel@users.noreply.github.com>
2020-10-25 20:11:16 +0100

    Update plantDataScreen.dart
    Finished layout, still some aligment to change but mostly full done

--- lib/screens/plantDataScreen.dart
+++ lib/screens/plantDataScreen.dart
@@ -7,6 +7,7 @@ import 'package:roslinki_politechnika/models/plantsListView.dart';
 import 'package:roslinki_politechnika/models/potDecoration.dart';
 import 'package:roslinki_politechnika/providers/plantsListProvider.dart';
 import 'package:roslinki_politechnika/providers/potDecorationProvider.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class PlantDataScreen extends StatefulWidget {
   static const String routeName = '/plantDataScreen';
@@ -34,6 +35,153 @@ class _PlantDataScreenState extends State<PlantDataScreen> {
         width: double.infinity,
         child: Stack(
           children: [
+            Positioned(
+              //! data in this container should be gathered from sensor
+              bottom: 0,
+              child: Container(
+                height: 300.h,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(70.h),
+                    //? added image in this region topLeft: Radius.circular(70.h),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 50.h,
+                          width: 10.h,
+                          child: Transform.rotate(
+                            angle: pi,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.grey,
+                                      width: 1.0.h,
+                                    ),
+                                    color: Color.fromRGBO(220, 220, 220, 1.0),
+                                    borderRadius: BorderRadius.circular(10.h),
+                                  ),
+                                ),
+                                FractionallySizedBox(
+                                  heightFactor:
+                                      0.4, //TODO make it gether the information about water left in tank
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.redAccent,
+                                      borderRadius: BorderRadius.circular(10.h),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        _textSchemePercent(
+                            10, true), //TODO change it later for value
+                        _textSchemePercent(78, true),
+                        _textSchemePercent(24, false),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _textSchemeLabels('Water tank', null),
+                        _textSchemeLabels('Light', '✓'),
+                        _textSchemeLabels('Temper.', '✓'),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: GestureDetector(
+                child: Container(
+                  height: 90.h,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(40.h),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.bar_chart_rounded,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Statistic',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.h,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: GestureDetector(
+                child: Container(
+                  height: 90.h,
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: FractionallySizedBox(
+                          heightFactor: 0.5,
+                        ),
+                      ),
+                      Text(
+                        'Information',
+                        style: TextStyle(
+                          fontSize: 20.h,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          width: 3.h,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).accentColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(3.h),
+                              topRight: Radius.circular(3.h),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
             Positioned(
               left: 20.h,
               top: 50.h,
@@ -89,6 +237,38 @@ class _PlantDataScreenState extends State<PlantDataScreen> {
               ),
             ),
             Positioned(
+              top: 110.h,
+              left: -160.h,
+              child: ClipOval(
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 450.h,
+                      width: 420.h,
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.black,
+                        strokeWidth: 3.h,
+                      ),
+                    ),
+                    Container(
+                      height: 450.h,
+                      width: 420.h,
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: Provider.of<PlantsManagement>(context,
+                                listen: false)
+                            .plants
+                            .firstWhere(
+                                (element) => element.id == widget.plantId)
+                            .urlImage,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
               top: 140.h,
               right: 20.h,
               child: Column(
@@ -110,58 +290,6 @@ class _PlantDataScreenState extends State<PlantDataScreen> {
                 ],
               ),
             ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                height: 300.h,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(70.h),
-                    topLeft: Radius.circular(70.h),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.h),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 40.h,
-                        width: 10.h,
-                        child: Transform.rotate(
-                          angle: pi,
-                          child: Stack(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.grey,
-                                    width: 1.0.h,
-                                  ),
-                                  color: Color.fromRGBO(220, 220, 220, 1.0),
-                                  borderRadius: BorderRadius.circular(10.h),
-                                ),
-                              ),
-                              FractionallySizedBox(
-                                heightFactor:
-                                    0.4, //TODO make it gether the information about water left in tank
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.redAccent,
-                                    borderRadius: BorderRadius.circular(10.h),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
           ],
         ),
       ),
@@ -178,12 +306,27 @@ Widget _potData(String dataName, BuildContext context, double values) {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              '$values%',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35.h,
-                  fontWeight: FontWeight.bold),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${values.toInt()}', //TODO change it later for value
+                  style: TextStyle(
+                      fontSize: 35.h,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  width: 5.h,
+                ),
+                Text(
+                  '%',
+                  style: TextStyle(
+                      fontSize: 30.h,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white),
+                ),
+              ],
             ),
             Text(
               ' $dataName',
@@ -199,6 +342,50 @@ Widget _potData(String dataName, BuildContext context, double values) {
         ),
         PotDecoration(
           dataName,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _textSchemePercent(double value, bool percent) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 25.h),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '${value.toInt()}', //TODO change it later for value
+          style: TextStyle(fontSize: 40.h, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          width: 5.h,
+        ),
+        Text(
+          percent ? '%' : '°C',
+          style: TextStyle(fontSize: 30.h, fontWeight: FontWeight.w100),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _textSchemeLabels(String value, String label) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 25.h),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          value,
+          style: TextStyle(fontSize: 20.h, fontWeight: FontWeight.normal),
+        ),
+        SizedBox(
+          width: 5.h,
+        ),
+        Text(
+          label != null ? '✓' : '',
+          style: TextStyle(fontSize: 20.h, fontWeight: FontWeight.bold),
         ),
       ],
     ),
